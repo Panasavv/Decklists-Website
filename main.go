75ae382978eabdbc2b8c0b20121d7462373c1aad
Panasavv-CERTH <panasavv@iti.gr>
2023-05-30 13:25:37 +0300

    First commit

--- main.go
+++ main.go
@@ -1,34 +1,10 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+import(
+	"Decklists-Website/cardReader"
 )
 
+
 func main() {
-	resp, err := http.Get("https://mtgjson.com/api/v5/ONE.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var generic map[string]interface{}
-	err = json.NewDecoder(resp.Body).Decode(&generic)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//Getting the date from meta
-	//	imerominia := generic["meta"].(map[string]interface{})["date"]
-	//	fmt.Println(imerominia)
-
-	data1 := generic["data"]
-	cards1 := data1.(map[string]interface{})["cards"]
-	firstCard := cards1.([]interface{})[0]
-	cardName := firstCard.(map[string]interface{})["name"]
-
-	fmt.Println(cardName)
-
-	//cardName := generic["data"].(map[string]interface{})["cards"].([]interface{})[0]
+	cardReader.getCardName()
 }
